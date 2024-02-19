97e1498b12602d0a5794e8b9fb8068b7e668114d
BLohny <rubycpp1225@outlook.kr>
2024-02-19 19:06:57 +0900

    m

---
+++ Java/programmers/Level1/문자열내마음대로정렬하기.java
@@ -0,0 +1,23 @@
+package Java.programmers.Level1;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class 문자열내마음대로정렬하기 {
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                char c1 = s1.charAt(n);
+                char c2 = s2.charAt(n);
+                if (c1 == c2) {
+                    return s1.compareTo(s2);
+                } else {
+                    return Character.compare(c1, c2);
+                }
+            }
+        });
+    
+        return strings;
+    }
+}
