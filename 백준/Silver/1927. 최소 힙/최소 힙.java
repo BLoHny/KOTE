765cbe5a9a01bd671cf5a484c3b8141b5c558731
BLoHny <rubycpp1225@outlook.kr>
2024-04-02 09:28:34 +0900

    [Silver II] Title: 최소 힙, Time: 1124 ms, Memory: 59364 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,28 +1,97 @@
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int len = sc.nextInt();
+    public ArrayList<Integer> elems = new ArrayList<>();
 
-        int obj = 0;
-        PriorityQueue<Integer> elems = new PriorityQueue<>();
+    public Main() {
+        elems.add(0);
+    }
 
-        for (int i = 0; i < len; i++) {
-            obj = sc.nextInt();
-            if (obj != 0) {
-                elems.add(obj);
-            } else {
-                if (!elems.isEmpty()) {
-                    System.out.println(elems.poll());
-                }
-                else
-                    System.out.println(0);
+    public void push(int value) {
+        elems.add(value);
+        up_swap(elems.size() - 1);
+    }
+
+    public void pop() {
+        if (elems.size() == 1) {
+            System.out.println(0);
+            return;
+        }
+
+        System.out.println(elems.get(1));
+        int last = elems.get(elems.size() - 1);
+        elems.set(1, last);
+
+        elems.remove(elems.size() - 1);
+        down_swap(1);
+    }
+
+    public void up_swap(int index) {
+        int parent = parent(index);
+
+        if (parent == 0) {
+            return;
+        }
+
+        if (elems.get(parent) <= elems.get(index)) {
+            return;
+        }
+
+        swap(parent, index, elems.get(parent), elems.get(index));
+        up_swap(parent);
+    }
+
+    public void swap(int... indexs) {
+        elems.set(indexs[0], indexs[3]);
+        elems.set(indexs[1], indexs[2]);
+    }
+
+    public void down_swap(int index) {
+
+        int leftIndex = leftChild(index);
+        int rightIndex = rightChild(index);
+
+        if (leftIndex < elems.size() && elems.get(leftIndex) < elems.get(index)) {
+            if (rightIndex >= elems.size() || elems.get(leftIndex) < elems.get(rightIndex)) {
+                swap(index, leftIndex, elems.get(index), elems.get(leftIndex));
+                down_swap(leftIndex);
+                return;
             }
         }
 
-        sc.close();
+        if (rightIndex < elems.size() && elems.get(rightIndex) < elems.get(index)) {
+            swap(index, rightIndex, elems.get(index), elems.get(rightIndex));
+            down_swap(rightIndex);
+        }
+    }
+
+    public int parent(int index) {
+        return index / 2;
+    }
+
+    public int leftChild(int index) {
+        return index * 2;
+    }
+
+    public int rightChild(int index) {
+        return index * 2 + 1;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int len = Integer.parseInt(br.readLine());
+        for (int i = 0; i < len; i++) {
+            int o = Integer.parseInt(br.readLine());
+            if (o == 0) {
+                main.pop();
+            } else
+                main.push(o);
+        }
+        br.close();
     }
 }
